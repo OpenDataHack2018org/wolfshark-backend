04ee29c0afcdabdc8725a5f989419e85103cb6da
Toby Such <toby@suchfamily.com>
2018-06-09 18:19:12 +0100

    Added function to generate all the grib files between two time periods using a specific interval.

--- cdsapi_wrapper.py
+++ cdsapi_wrapper.py
@@ -1,5 +1,15 @@
 import cdsapi
 import os
+import datetime
+
+def get_grib_files(job_id, start_date, start_time, end_date, end_time, interval):
+    current_datetime = datetime.datetime(start_date.year, start_date.month, start_date.day, hour=start_time.hour)
+    end_datetime = datetime.datetime(end_date.year, end_date.month, end_date.day, hour=end_time.hour)
+    interval_timedelta = datetime.timedelta(hours=interval)
+    while current_datetime <= end_datetime:
+        print("Looking up " + str(current_datetime))
+        make_request(job_id, str(current_datetime.year), str(current_datetime.month), str(current_datetime.day), str(current_datetime.hour) + ":00")
+        current_datetime += interval_timedelta
 
 def make_request(job_id, year, month, day, time):
 
--- server.py
+++ server.py
@@ -1,8 +1,16 @@
 import cdsapi_wrapper as cds
+import datetime
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    cds.make_request("1", "2009", "03", "21", "14:00")
+    start_date = datetime.date(2009, 3, 20)
+    start_time = datetime.time(14, 0, 0)
+    end_date = datetime.date(2009, 3, 20)
+    end_time = datetime.time(16, 0, 0)
+    interval = 1
+    
+    
+    cds.get_grib_files("1", start_date, start_time, end_date, end_time, interval)
     return 'Hello, World!'
