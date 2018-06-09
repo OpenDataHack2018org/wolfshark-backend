8b8417962d5bfc05889f302a6a851f706b51f0b0
rogermilroy <rogermilroy@icloud.com>
2018-06-09 20:55:37 +0100

    changed to flask-restful

--- server.py
+++ server.py
@@ -1,6 +1,21 @@
 from flask import Flask
-app = Flask(__name__)
+from flask_restful import reqparse, Resource, Api
+from job import Job
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+app = Flask(__name__)
+api = Api(app)
+
+parser = reqparse.RequestParser()
+parser.add_argument()
+
+class AddJob(Resource):
+    def post(self):
+
+
+        return {'hello': 'world'}
+
+
+api.add_resource(AddJob, '/api/job')
+
+if __name__ == '__main__':
+    app.run(debug=True)
