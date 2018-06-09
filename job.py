04b910fd0c4238f9451daa02a0066ca36cf83342
rogermilroy <rogermilroy@icloud.com>
2018-06-09 19:44:49 +0100

    add enums for status and theme

--- job.py
+++ job.py
@@ -1,23 +1,31 @@
 from peewee import *
 
+STATUS_QUEUED = 1
+STATUS_PROCESSING = 2
+STATUS_COMPLETED = 3
+STATUS_ERROR = 4
+
+THEME_DARK = 5
+THEME_LIGHT = 6
+
 db = SqliteDatabase('jobs.db')
 
 
 class Job(Model):
     job_id = AutoField()
-    title = CharField()                 # needs to be an enum
+    title = CharField()
     user_name = CharField()
     user_key = CharField()
     start_date_time = DateTimeField()
     end_date_time = DateTimeField()
     interval = IntegerField()
     no_of_frames = IntegerField()
-    area = CharField()                  # needs to be an enum
-    theme = CharField()                 # needs to be an enum
+    area = CharField()                     # needs to be an enum
+    theme = IntegerField()
     fps = IntegerField()
-    status = CharField()                # needs to be an enum
+    status = IntegerField()
 
-    def __init__(self, title, user_name, user_key, start_date_time, end_date_time, interval, no_of_frames, area, theme, fps, status):
+    def __init__(self, title, user_name, user_key, start_date_time, end_date_time, interval, no_of_frames, area, theme, fps):
         Model.__init__(self)
         self.title = title
         self.user_name = user_name
@@ -29,7 +37,7 @@ class Job(Model):
         self.area = area
         self.theme = theme
         self.fps = fps
-        self.status = status
+        self.status = STATUS_QUEUED
 
     class Meta:
         database = db
